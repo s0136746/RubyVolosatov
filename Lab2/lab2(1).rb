324b47a9566dd801269972def24f70e66d738d5b
Yaroslav <s0136746@edu.kubsu.ru>
2021-04-06 15:08:19 +0300

    izmeneniya

--- Lab2/lab2(1).rb
+++
@@ -1,2 +0,0 @@
-array = gets.chonp
-puts array
---
+++ lr3.1.rb
@@ -0,0 +1,14 @@
+def vvod(element)
+    element = element.to_i
+    myarray = Array.new
+
+    puts "Введите элементы списка: "
+    for el in 1..element
+        el = STDIN.gets.chomp
+        myarray.unshift(el)
+    end
+
+    return myarray
+end
+
+print vvod(ARGV[0])
