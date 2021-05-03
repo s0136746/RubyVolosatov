29a8a916b1a111f6751fe8b3b9bd011b5831ab22
Yaroslav <s0136746@edu.kubsu.ru>
2021-05-03 15:09:07 +0300

    method50

--- Lab2/iz2(7).rb
+++ Lab2/iz2(7).rb
@@ -6,8 +6,7 @@ def method14(a, b, arr)
     return arr[a..b].size
 end
 
-def method26()
-
+def method26(arr)
 
 
 end
@@ -18,19 +17,27 @@ def method38()
 
 end
 
-def method50()
+def method50(l1,l2)
 
-
+    c = (l1 | l2) - (l1 & l2)
+    return c
 
 end
 
-print ("Введите элементы списка: ")
-arr = gets.chomp.split
-arr = arr.map(&:to_i)
+# print ("Введите элементы списка: ")
+# arr = gets.chomp.split
+# arr = arr.map(&:to_i)
 # puts method2(arr)
-puts 'Необходимо ввести интервал'
-		print 'a = '
-		a = gets.to_i
-		print 'b = '
-		b = gets.to_i
-puts method14(a, b, arr)
+# puts 'Необходимо ввести интервал'
+# 		print 'a = '
+# 		a = gets.to_i
+# 		print 'b = '
+# 		b = gets.to_i
+# puts method14(a, b, arr)
+print ("Введите элементы 1 списка: ")
+l1 = gets.chomp.split
+l1 = l1.map(&:to_i)
+print ("Введите элементы 2 списка: ")
+l2 = gets.chomp.split
+l2 = l2.map(&:to_i)
+puts method50(l1,l2)
