a38c33a881ecbf4aa303290b8fb747ba6f5d1714
Yaroslav <s0136746@edu.kubsu.ru>
2021-05-03 14:33:55 +0300

    method 2

--- Lab2/iz2(7).rb
+++ Lab2/iz2(7).rb
@@ -2,11 +2,8 @@ def method2(arr)
     arr.each_with_index.min
 end
 
-def method14()
-
-
-
-
+def method14(a, b, arr)
+    return arr[a..b].size
 end
 
 def method26()
@@ -30,4 +27,10 @@ end
 print ("Введите элементы списка: ")
 arr = gets.chomp.split
 arr = arr.map(&:to_i)
-puts method2(arr)
+# puts method2(arr)
+puts 'Необходимо ввести интервал'
+		print 'a = '
+		a = gets.to_i
+		print 'b = '
+		b = gets.to_i
+puts method14(a, b, arr)
